fd5c7ed6f1ba5614008af364976cbfd37bc7c747
Lokrion <m.kostian@yandex.ru>
2022-08-19 01:09:19 +0700

    add example 2

---
+++ example 2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Input first integer number:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second integer number:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 == num2 * num2)
+{
+    Console.WriteLine($"{num1} is a quad of {num2}");
+}
+else
+{
+    Console.WriteLine($"{num1} is not a quad of {num2}");
+}
