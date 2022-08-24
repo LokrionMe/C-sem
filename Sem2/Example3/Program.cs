618e42fc3f49a03c6e3011526fddb57baa7b807a
Lokrion <m.kostian@yandex.ru>
2022-08-24 21:35:54 +0700

    example 3

---
+++ Sem2/Example3/Program.cs
@@ -0,0 +1,10 @@
+﻿void Dev2on1(int num1, int num2)
+{
+    if (num2 % num1 == 0) Console.WriteLine("Yes");
+    else Console.WriteLine(num2 % num1);
+}
+int num1 = new Random().Next(1, 10);
+int num2 = new Random().Next(10, 20);
+Console.WriteLine(num1);
+Console.WriteLine(num2);
+Dev2on1(num1, num2);
