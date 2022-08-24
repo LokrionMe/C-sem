5d6f8c7d76df33b9a4911a4f609c2933581d78f9
Lokrion <m.kostian@yandex.ru>
2022-08-24 21:59:38 +0700

    example 4

---
+++ Sem2/Example4/Program.cs
@@ -0,0 +1,8 @@
+﻿bool ChekNumber(int number)
+{
+    if ((number % 7 == 0) & (number % 23 == 0)) return true;
+    return false;
+}
+int number = new Random().Next(100, 1000);
+Console.WriteLine(number);
+Console.WriteLine(ChekNumber(number));
