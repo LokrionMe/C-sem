beebd4ee626a207563a3e773d22f7f9dfad731ce
Lokrion <m.kostian@yandex.ru>
2022-08-24 21:08:29 +0700

    example 1

---
+++ Sem2/Example1/Program.cs
@@ -0,0 +1,14 @@
+﻿int FindBiggerDigit(int number)
+{
+    int ed = number % 10;
+    int dec = number / 10;
+
+    int result;
+    if(ed > dec) result = ed;
+    else result = dec;
+
+    return result; 
+}
+
+int randNumber = new Random().Next(10, 100);
+Console.WriteLine($"Bigger digit of number {randNumber} is {FindBiggerDigit(randNumber)}");
