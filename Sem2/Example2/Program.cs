6818b4c216ea040f64a7c3f668e54b2b08563412
Lokrion <m.kostian@yandex.ru>
2022-08-24 21:19:47 +0700

    example 2

---
+++ Sem2/Example2/Program.cs
@@ -0,0 +1,10 @@
+﻿int Doing2From3(int number)
+{
+    int ed = number % 10;
+    int hun = number / 100;
+    int result = hun * 10 + ed;
+    return result;
+}
+
+int randNumber = new Random().Next(100, 1000);
+Console.WriteLine($"Bigger digit of number {randNumber} is {Doing2From3(randNumber)}");
