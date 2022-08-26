02454b4e44d4506c61a5caedb504ba3d00edcc29
Lokrion <m.kostian@yandex.ru>
2022-08-26 20:40:39 +0700

    sem3 example3

---
+++ Sem3/Example3/Program.cs
@@ -0,0 +1,15 @@
+﻿double LenSegment(double x1, double y1, double x2, double y2)
+{
+    double z = (x2 - x1)*(x2 - x1)+(y2 - y1)*(y2 - y1);
+    double result = Math.Sqrt(z);
+    return result;
+}
+Console.Write("Input x1:");
+double xA = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input x2:");
+double xB = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input y1:");
+double yA = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input y2:");
+double yB = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($"Len = {LenSegment(xA, yA, xB, yB)} ");
