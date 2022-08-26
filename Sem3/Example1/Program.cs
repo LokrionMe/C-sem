2ba40fefdf7d837eb827703bdf65de8e903bc5bb
Lokrion <m.kostian@yandex.ru>
2022-08-26 20:06:09 +0700

    sem3 example1

---
+++ Sem3/Example1/Program.cs
@@ -0,0 +1,18 @@
+﻿void SearchChapter(double x, double y)
+{
+    if (x>0)
+    {
+        if (y>0) Console.WriteLine("1 chapter");
+        else Console.WriteLine("4 chapter");
+    }
+    else
+    {
+        if (y>0) Console.WriteLine("2 chapter");
+        else Console.WriteLine("3 chapter");
+    }
+}
+Console.Write("Input x:");
+double x = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input y:");
+double y = Convert.ToDouble(Console.ReadLine());
+SearchChapter(x, y);
