d22a5b31669ba6491c319b6ae2255fb506b7a219
Lokrion <m.kostian@yandex.ru>
2022-08-31 18:43:49 +0700

    lesson4ex2

--- Sem4/Example1/Program.cs
+++ Sem4/Example1/Program.cs
@@ -3,7 +3,7 @@
     int sum = 0;
     for (int i = 1; i < num; i++) 
     {
-        sum = sum + i;
+        sum += i;
     }
     Console.WriteLine($"Summa ot 1 do {num} = {sum}");
 }
---
+++ Sem4/Example2/Program.cs
@@ -0,0 +1,13 @@
+﻿void NumberOfNumber(int num)
+{
+    int i = 1;
+    while (num / 10 != 0) 
+    {
+        i += 1;
+        num /= 10;
+    }
+    Console.WriteLine(i); 
+}
+Console.Write("Vvedite celoe chislo: ");
+int n = Convert.ToInt32(Console.ReadLine());
+NumberOfNumber(n);
