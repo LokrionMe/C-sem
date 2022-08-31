08faf48cca7bd218ed8bac4323e5332f86ee96b6
Lokrion <m.kostian@yandex.ru>
2022-08-31 18:51:09 +0700

    lesson4ex3

---
+++ Sem4/Example3/Program.cs
@@ -0,0 +1,12 @@
+﻿int fact (int num)
+{
+    int pr = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        pr *= i;
+    }
+    return pr;
+}
+Console.Write("Vvedite celoe chislo: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Factorial = {fact(n)}");
