af9a9110040d4700ada22f86c9452b86fb900d33
Lokrion <m.kostian@yandex.ru>
2022-09-03 10:50:06 +0700

    sem5 ex2

---
+++ Sem5/ex2/Program.cs
@@ -0,0 +1,36 @@
+﻿int [] CreateRandomArray(int n, int minVal, int maxVal)
+{
+    int [] arr = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = new Random().Next(minVal,maxVal + 1);
+    }
+    return arr;
+}
+void PrintArray(int [] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.WriteLine("\b\b]");
+}
+int [] ChangeZnak (int[] array)
+{
+    int [] chArray = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        chArray[i] = array[i] * (-1);
+    }
+    return chArray;
+}
+Console.WriteLine("Input size of array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input minimal number: ");
+int minVal = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input maximum number: ");
+int maxVal = Convert.ToInt32(Console.ReadLine());
+int [] newArr = CreateRandomArray(size, minVal, maxVal);
+PrintArray(newArr);
+PrintArray(ChangeZnak(newArr));
